62dbc1d7caa154fa098b43272adbfc6606fb9b60
Diego EM <dmunozaguilar2000@gmail.com>
2022-03-25 13:37:28 -0600

    big card component added

---
+++ client/src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from 'src/app/models';
+import { ProductsService } from 'src/app/services/products.service';
+
+@Component({
+  selector: 'big-card',
+  templateUrl: './big-card.component.html',
+  styleUrls: ['./big-card.component.sass']
+})
+export class BigCardComponent implements OnInit {
+
+  @Input() id: string|number = 1;
+  @Input() reverse: boolean = false;
+  productDetails?: Product;
+
+  constructor(private connect: ProductsService) { }
+
+  ngOnInit(): void {
+    this.connect.getProduct(this.id)
+      .subscribe(data => this.productDetails = data);
+  }
+
+}
