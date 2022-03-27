8c1e56368c7fe88ae3e6cd5e3e2310a336b44f5c
Diego EM <dmunozaguilar2000@gmail.com>
2022-03-27 13:59:45 -0600

    UI done

--- client/src/app/pages/product-view/product-view.component.ts
+++ client/src/app/pages/product-view/product-view.component.ts
@@ -11,6 +11,7 @@ import { Product } from 'src/app/models';
 export class ProductViewComponent implements OnInit {
 
   currentProduct!: Product;
+  moreProducts: number[] = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -24,6 +25,25 @@ export class ProductViewComponent implements OnInit {
         this.connect.getProduct(id)
           .subscribe((product: Product) => this.currentProduct = product);
     });
+    this.refreshRecomendations();
   }
 
+  getRandomNumber(): number{
+    let random = Math.ceil(Math.random() * 24);
+    return random;
+  }
+
+  refreshRecomendations(): void{
+    this.moreProducts = [];
+    for (let i = 0; i < 4; i++){
+      this.moreProducts.push(this.getRandomNumber());
+    }
+  }
+
+  scrollToTop(): void{
+    window.scroll({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
 }
