c44af96033d293e37727d2f4c453216576f90808
Diego-EM <dmunozaguilar2000@gmail.com>
2022-03-29 16:37:31 -0600

    cart-overview page addded

---
+++ client/src/app/pages/cart-overview/cart-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-overview',
+  templateUrl: './cart-overview.component.html',
+  styleUrls: ['./cart-overview.component.sass']
+})
+export class CartOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
