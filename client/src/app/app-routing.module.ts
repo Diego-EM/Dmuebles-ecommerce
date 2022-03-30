5d99584fd8a87652816d91733671dd32fbd32f83
Diego-EM <dmunozaguilar2000@gmail.com>
2022-03-30 12:32:47 -0600

    about page addded

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProductViewComponent } from './pages/product-view/product-view.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { CartOverviewComponent } from './pages/cart-overview/cart-overview.component';
+import { PageInfoComponent } from './pages/page-info/page-info.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'product/:id', component: ProductViewComponent},
   {path: 'search/:req', component: ProductListComponent},
-  {path: 'car', component: CartOverviewComponent}
+  {path: 'car', component: CartOverviewComponent},
+  {path: 'about', component: PageInfoComponent}
 ];
 
 @NgModule({
